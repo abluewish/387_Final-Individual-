6d188dacd884a647525452b3b982581625540151
abluewish <35025796+abluewish@users.noreply.github.com>
2019-05-18 08:18:33 -0400

    Create ML.py

---
+++ ML.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+np.random.seed(0)
+n = 15
+x = np.linspace(0,10,n) + np.random.randn(n)/5
+y = np.sin(x)+x/6 + np.random.randn(n)/10
+
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, random_state=0)
